603a3b0dd846549b6ca0e60172247e035f666df0
Aquiej12 <abnerquiej12@gmail.com>
2026-02-16 17:18:42 -0600

    ola alma
    si revisas mi github o nomas es para asustarnos ?

--- Prelab03/Prelab03/main.asm
+++ Prelab03/Prelab03/main.asm
@@ -1,26 +1,26 @@
 /*
-* laboratorio2-Botones y Timer 0
+* Prelaboratorio3-Interrupciones
 *
 * Creado: 16/02/2026 - 12:00
 * Autor : Abner Quiej
 * Descripcion: Contador binario de 4 bits del prelaboratorio
-para que el contador se incrementará cada 1s. Importante, el timer0 seguirá
-incrementando cada 100ms.
-En el momento que el contador de segundos sea igual al contador de los botones,
-deberá reiniciar el contador de segundos y cambiar el estado de una LED (de 0-
->1 o de 1->0 durante un ciclo entero). De manera que pueda variar el periodo del
-encendido y apagado del indicador utilizando los botones.
+para que el contador se incrementará cada interrupcion
 
 */
 /****************************************/
 // Encabezado (Definici n de Registros, Variables y Constantes)?
 .include "M328PDEF.inc" // Include definitions specific to ATMega328P
+.def counter = R20        ; Registro para el contador de 4 bits
 .dseg
 .org SRAM_START
 //variable_name: .byte 1 // Memory alocation for variable_name: .byte (byte size)
 .cseg
 .org 0x0000
 	RJMP RESET
+
+.org PCI1addr 
+    RJMP ISR_PCINT1
+
 
  .org OVF0addr
 	RJMP TIMER0_OVF
@@ -39,14 +39,6 @@ OUT SPH, R16
 
 
 // Configurar entradas y salidas
-
-//Habilitar cambio del prescaler (ventana de 4 ciclos)
-LDI R16, (1<<CLKPCE)
-STS CLKPR, R16
-
-//Configurar división entre 8
-LDI R16, 0b00000011    ; CLKPS = 0011 ? divide entre 8
-STS CLKPR, R16
 
 // Input -> PB4, PB5, PC5
 CBI DDRC, DDC4 // Poniendo en 0, el bit 4 de DDRC -> Input
@@ -77,11 +69,16 @@ CBI PORTB, PORTB2   // Inicialmente apagado el PB2
 CBI PORTB, PORTB3   // Inicialmente apagado el PB3
 CBI PORTB, PORTB4   // Inicialmente apagado el PB4
 
-
-//CALCULANDO EL PRESCALER.
+; Habilitar el grupo PCIE1 (Puerto C)
+    LDI R17, (1 << PCIE1)
+    STS PCICR, R17
+    // Habilitar específicamente los pines PCINT11 (PC3) y PCINT12 (PC4)
+    LDI R17, (1 << PCINT11) | (1 << PCINT12)
+    STS PCMSK1, R17
+	//CALCULANDO EL PRESCALER.
 	//CON:
 		//	- TIMER0=8BITS
-		//	- Fclk=2000000 Hz
+		//	- Fclk=16000000 Hz
 		//	- PreScaler=1024
 		//	- Overflow==0.009984 s
 
@@ -90,10 +87,10 @@ CBI PORTB, PORTB4   // Inicialmente apagado el PB4
     LDI R16, 0x00
     OUT TCCR0A, R16
 
-	LDI R16, 60
+	LDI R16, 99
 	OUT TCNT0, R16
 	CLR R16
-	LDI R17, 10      ; 10 interrupciones = 1 segundo
+	LDI R18, 100      ; 100 interrupciones = 1 segundo
 
     ; Prescaler = 1024 (CS02=1, CS00=1)
     LDI R16, (1<<CS02)|(1<<CS00)
@@ -102,6 +99,7 @@ CBI PORTB, PORTB4   // Inicialmente apagado el PB4
     ; Habilitar interrupción por overflow
     LDI R16, (1<<TOIE0)
     STS TIMSK0, R16
+
     ; Habilitar interrupciones globales
     SEI
 
@@ -120,7 +118,7 @@ CLR R1
 
 
 MAIN_LOOP:
-	ANDI R19, 0b00001111	// solo queremos cuenta de 0 a 15
+ANDI R19, 0b00001111	// solo queremos cuenta de 0 a 15
 	LDI ZH, HIGH(disp7seg<<1)
 	LDI ZL, LOW(disp7seg<<1)
 
@@ -129,84 +127,61 @@ MAIN_LOOP:
 
 	LPM R23, Z				// Guardar lo apuntado en z
 	OUT PORTD, R23			// Mostrar lo apuntado en z
-
-	
-	MOV R21, R18
-	ANDI R21, 0b00001111
-	CP R21, R19
-	BRNE LEERBOTONES
-	SBI PINB, PB4
-	CLR R18
-
-
-
-LEERBOTONES:
-IN R20, PINC
-
-; PC3 = INC
-SBIS PINC, PC3
-RJMP ANTRBT_INC
-
-; PC4 = DEC
-SBIS PINC, PC4
-RJMP ANTRBT_DEC
-
-RJMP MAIN_LOOP
-
-
-
-
-ANTRBT_INC:
-    ; Delay ~20ms
-    LDI R24, 50
-DELAY1I:
-    LDI R25, 255
-DELAY2I:
-    DEC R25
-    BRNE DELAY2I
-    DEC R24
-    BRNE DELAY1I
-
-    ; Confirmar si sigue presionado (debe ser 0 para seguir)
-    SBIC PINC, PC3       ; Si se soltó (es 1), salta el incremento
-    RJMP MAIN_LOOP       ; Regresa al loop si fue un ruido
-
-    INC R19              ; Incrementa el contador principal
-
-ESPERAR_SUELTE_INC:
-    SBIS PINC, PC3       ; Si es 1 (suelto), salta el bucle de espera
-    RJMP ESPERAR_SUELTE_INC
-
-    RJMP MAIN_LOOP
-
-
-ANTRBT_DEC:
-    ; Delay ~20ms
-    LDI R24, 50
-DELAY1D:
-    LDI R25, 255
-DELAY2D:
-    DEC R25
-    BRNE DELAY2D
-    DEC R24
-    BRNE DELAY1D
-
-    ; Confirmar si sigue presionado (debe ser 0)
-    SBIC PINC, PC4       ; Si se soltó (es 1), salta el decremento
-    RJMP MAIN_LOOP
-
-    DEC R19              ; Decrementa el contador principal
-
-ESPERAR_SUELTE_DEC:
-    SBIS PINC, PC4       ; Si es 1 (suelto), salta el bucle de espera
-    RJMP ESPERAR_SUELTE_DEC
-
-    RJMP MAIN_LOOP
-
-
+	OUT PORTB, counter
+	RJMP MAIN_LOOP
 ///****************************************/
 //// Interrupt routines
 ///****************************************/
+ISR_PCINT1:
+    ; Guardar contexto
+    PUSH R16
+    IN   R16, SREG
+    PUSH R16
+	PUSH R24
+	PUSH R25
+	PUSH R21
+
+//ANTIRREBOTE
+LDI  R24, 255
+DELAY_ISR:
+    LDI  R25, 255
+DELAY_INNER:
+    DEC  R25
+    BRNE DELAY_INNER
+    DEC  R24
+    BRNE DELAY_ISR
+
+    ; Leer el estado de los pines (Activos en BAJO por pull-up)
+    IN R17, PINC
+    
+    ; Debido a que la interrupción ocurre en ambos flancos (0->1 y 1->0),
+    ; verificamos si el botón está presionado (es decir, el bit es 0).
+    
+CHECK_INC:
+    SBRC R17, PORTC3     ; Si PC3 es 0, salta la siguiente instrucción
+    RJMP CHECK_DEC        ; Si es 1 (suelto), saltar a revisar el otro
+    ; Acción Incrementar
+    INC counter
+    RJMP EXIT_ISR
+
+CHECK_DEC:
+    SBRC R17, PORTC4     ; Si PC4 es 0, salta la siguiente instrucción
+    RJMP EXIT_ISR         ; Si es 1 (suelto), salir
+    ; Acción Decrementar
+    DEC counter
+
+
+EXIT_ISR:
+	POP  R21
+	POP  R25
+	POP	 R24
+    POP  R16
+    OUT  SREG, R16
+    POP  R16
+    RETI
+
+//Interrupcion por Timer0
+
 TIMER0_OVF:
     ; Guardar contexto
     PUSH R16
@@ -215,23 +190,14 @@ TIMER0_OVF:
 	PUSH R24
 	PUSH R25
 	PUSH R21
-	; Recargar 60 para mantener 100 ms
-    LDI  R16, 60
+	; Recargar 99 para mantener 100 ms
+    LDI  R16, 99
     OUT  TCNT0, R16
+	DEC  R18
+	BRNE REGRESAR		   // Si no es 0, salir
+    LDI  R18, 100			//nuevamente contar 0.01s 100 veces = 1segundo
+	INC R19
 
-    ; Toggle PB0-PB3
-	DEC  R17          ; Decrementar contador
-    BRNE REGRESAR		   ; Si no es 0, salir
-    LDI  R17, 10
-	INC R18
-	IN R16, PORTB     ; Leer estado actual (incluyendo el LED PB4)
-    ANDI R16, 0xF0    ; Limpiar bits 0-3 (contador viejo), mantener 4-7
-    MOV R21, R18
-    ANDI R21, 0x0F    ; Asegurar que R18 solo tenga 4 bits
-    OR R16, R21       ; Combinar LED actual con nuevo conteo
-    OUT PORTB, R16    ; Sacar al puerto
-	CLR R16
-    ; Restaurar contexto
 	REGRESAR:
 	POP  R21
 	POP  R25
